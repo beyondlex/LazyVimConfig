b742f8c8c914e92683a28b10aa3267899f315bb9
lex <lex@work.cn>
2024-06-24 17:22:22 +0800

    autocmd for lua file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,17 @@
 --
 
 -- vim.lsp.set_log_level("debug")
+--
+
+local function set_lua_keymaps()
+  local modes = { "n", "v" }
+  for _, mode in ipairs(modes) do
+    -- send lua script in current buffer to lua, and run
+    vim.api.nvim_buf_set_keymap(0, mode, "<M-Enter>", ":w !lua<CR>", { noremap = true, silent = true })
+  end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = set_lua_keymaps,
+})
