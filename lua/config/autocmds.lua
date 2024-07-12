be4082cded536229445da928ea7afab62c02820f
lex <lex@work.cn>
2024-07-12 17:16:25 +0800

    go

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,14 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   callback = set_lua_keymaps,
 })
+
+
+-- https://github.com/ray-x/go.nvim
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
