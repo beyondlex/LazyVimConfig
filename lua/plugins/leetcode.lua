40d33279391c2faff629f94f0525931495ae80f1
lex <lex@work.cn>
2024-07-12 17:16:32 +0800

    leetcode

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,23 @@
+return {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+      cn = {
+        enabled = true,
+        -- translator = true,
+        -- translate_problems = true,
+      },
+      -- gr_user_id=f96863f0-b3de-4035-950d-88767e02ed7e; _bl_uid=8wlgdwwI4kml2tm77ck47jszj7Uj; csrftoken=iMdBvuR39Y7LTVFHo0EmOfLeDxUSrSZgL5QkhAeaZg4RAWSXJdc2bhszxMkzTzuk; a2873925c34ecbd2_gr_last_sent_cs1=beyondsnk; Hm_lvt_fa218a3ff7179639febdb15e372f411c=1715582749; aliyungf_tc=2af660b2cbac56bfd4dc9734ac9e7722d10cbf3d0709c9518d287687c2db5219; a2873925c34ecbd2_gr_session_id=54d3385f-58ab-42bc-99ef-ff6ffed4bf68; a2873925c34ecbd2_gr_last_sent_sid_with_cs1=54d3385f-58ab-42bc-99ef-ff6ffed4bf68; Hm_lvt_f0faad39bcf8471e3ab3ef70125152c3=1720504405; HMACCOUNT=9AA7728C802AB33F; a2873925c34ecbd2_gr_session_id_sent_vst=54d3385f-58ab-42bc-99ef-ff6ffed4bf68; _gid=GA1.2.1134015835.1720504406; tfstk=fgw-T-wdRiKJpPC3Fu10xBDDaMsci8Erkzr6K20kOrUYuPFkKakovp3Y8X05aauLJl4DKLD7-yLLrr07JkSrA2aQJ2VliOqz4vkCSWBGIuzf3Qa0evT3c_KkaJ7GIOq7xSZLBNjUWlXm-DMIVXGBGintYvOWFygjGciMPvMQRosxAcdSRDGIhtirVe8CyqKS-pFpac93SFgXdpw5Lb3T2BvBd-n-wzqSk3xuH0h-5j4bjGeTfuwZvJS692E4Nzm4JOQxC5zYpfa1oKUE6Wwoh8_fCjlQhribrMAEDWEQQYlyYpzLjzeKszXfVykSAylaKNJrPkZaQ5DOeFo8GlUxs-YDQgoUIRBPfF0txQsADBRENmVMUCVmIuxUkm3G2YdeTjSqDVjvYBRENgixSgBXTBlcu; wechat_state=rYRrgzOI; LEETCODE_SESSION=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuZXh0X2FmdGVyX29hdXRoIjoiL3Byb2JsZW1zZXQvIiwiX2F1dGhfdXNlcl9pZCI6IjEwMTI4OTciLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjY1MDJhMmVlMDVkODdhZmYwYjRlOTlmNWM4ZjJkNDI2YmY4MGQ5MDAyYjRkNDdkNjEwMGRiODFmNDZiNzE4NjkiLCJpZCI6MTAxMjg5NywiZW1haWwiOiJiZXlvbmRzbmtAMTI2LmNvbSIsInVzZXJuYW1lIjoiYmV5b25kc25rIiwidXNlcl9zbHVnIjoiYmV5b25kc25rIiwiYXZhdGFyIjoiaHR0cHM6Ly9hc3NldHMubGVldGNvZGUuY24vYWxpeXVuLWxjLXVwbG9hZC91c2Vycy9NaDE0WjdlUVZuL2F2YXRhcl8xNTczODcxMjU3LnBuZyIsInBob25lX3ZlcmlmaWVkIjp0cnVlLCJpcCI6IjE0LjIzLjQ2LjIyNiIsIl90aW1lc3RhbXAiOjE3MjA1MDQ0MjYuMDk4NDg1MiwiZXhwaXJlZF90aW1lXyI6MTcyMzA1NzIwMCwidmVyc2lvbl9rZXlfIjoxLCJkZXZpY2VfaWQiOiJkYjJhNTg2N2ViM2U4ZWIwYjc5YWYwY2I0MGY1YTEwZCJ9.RLSLCvPXQ4CAGNGtt_pllpn9uH9xZVrt33M3NrpR988; a2873925c34ecbd2_gr_cs1=beyondsnk; Hm_lpvt_f0faad39bcf8471e3ab3ef70125152c3=1720504629; _ga=GA1.1.1123392551.1715582702; _ga_PDVPZYN3CW=GS1.1.1720504405.3.1.1720504812.60.0.0
+    },
+}
